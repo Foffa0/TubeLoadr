4c422bdd7a11be6bc850989de3149e836ea720ee
Foffa <schmuck.christopher@gmail.com>
2023-07-06 12:04:13 +0200

    NavigationService

--- YoutubeDownloader/Commands/NavigateCommand.cs
+++ YoutubeDownloader/Commands/NavigateCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YoutubeDownloader.Services;
 using YoutubeDownloader.Stores;
 using YoutubeDownloader.ViewModels;
 
@@ -10,18 +11,16 @@ namespace YoutubeDownloader.Commands
 {
     internal class NavigateCommand : CommandBase
     {
-        private readonly NavigationStore _navigationStore;
-        private readonly Func<ViewModelBase> _createViewModel;
+        private readonly NavigationService _navigationService;
 
-        public NavigateCommand(NavigationStore navigationStore, Func<ViewModelBase> createViewModel)
+        public NavigateCommand(NavigationService navigationService)
         {
-            _navigationStore = navigationStore;
-            _createViewModel = createViewModel;
+            _navigationService = navigationService;
         }
 
         public override void Execute(object? parameter)
         {
-            _navigationStore.CurrentViewModel = _createViewModel();
+            _navigationService.Navigate();
         }
     }
 }
---
+++ YoutubeDownloader/Services/NavigationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeDownloader.Stores;
+using YoutubeDownloader.ViewModels;
+
+namespace YoutubeDownloader.Services
+{
+    internal class NavigationService
+    {
+
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<ViewModelBase> _createViewModel;
+
+        public NavigationService(NavigationStore navigationStore, Func<ViewModelBase> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public void Navigate() 
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+    }
+}
