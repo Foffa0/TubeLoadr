9aaaefde2d050c77781cdf9a1975f81c3df14af9
Foffa <schmuck.christopher@gmail.com>
2023-07-19 20:04:18 +0200

    Add id to video view models to filter video lists by video-id

--- YoutubeDownloader/ViewModels/VideoViewModel.cs
+++ YoutubeDownloader/ViewModels/VideoViewModel.cs
@@ -11,6 +11,7 @@ namespace YoutubeDownloader.ViewModels
     {
         private readonly Video _video;
 
+        public Guid Id => _video.Id;
         public string Title => _video.Title;
         public int Duration => _video.Duration;
         public string Thumbnail => _video.Thumbnail;
