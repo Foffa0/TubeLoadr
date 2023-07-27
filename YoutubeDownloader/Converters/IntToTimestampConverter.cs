f8c94927b2ed4ddaf2eaf6a78c2d1d824cac238b
Foffa <schmuck.christopher@gmail.com>
2023-07-27 21:57:24 +0200

    Add converters for the download view

---
+++ YoutubeDownloader/Converters/IntToTimestampConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Diagnostics;
+
+namespace YoutubeDownloader.Converters
+{
+    internal class IntToTimestampConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan result = TimeSpan.FromSeconds((int)value);
+            string fromTimeString = result.ToString("hh':'mm':'ss");
+            return fromTimeString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
