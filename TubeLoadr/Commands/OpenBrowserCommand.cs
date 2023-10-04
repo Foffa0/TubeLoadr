cf9e3120619ac0a1051ce8a184f00957f05d8387
Foffa <schmuck.christopher@gmail.com>
2023-10-04 22:47:51 +0200

    Add Command to open links in the browser.

---
+++ TubeLoadr/Commands/OpenBrowserCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace TubeLoadr.Commands
+{
+    /// <summary>
+    /// Opens a given url (through the CommandParameter) in the default browser.
+    /// </summary>
+    internal class OpenBrowserCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            ProcessStartInfo ProcessStartInfo = new ProcessStartInfo
+            {
+                FileName = (string)parameter,
+                UseShellExecute = true
+            };
+            Process.Start(ProcessStartInfo);
+        }
+    }
+}
